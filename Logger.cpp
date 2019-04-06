db9070c57f6e89fd470dd11dd1c88f0dac6be6a4
Daniel Copley <djcopley@users.noreply.github.com>
2019-04-05 17:35:19 -0700

    Updated logger classes
    Signed-off-by: djcopley <djrcopley@icloud.com>

--- Logger.cpp
+++ Logger.cpp
@@ -6,10 +6,12 @@
 
 void Logger::registerLogger(LoggerBase *logger)
 {
-//    this->loggers.push_back(logger);
+    this->loggers.push_back(logger);
 }
 
 void Logger::logMessage(LOG_LEVEL msgLogLevel, String message)
 {
-
+    for (int i = 0; i < this->loggers.size(); i++) {
+        this->loggers[i]->logMessage(msgLogLevel, message);
+    }
 }
--- LoggerBase.h
+++ LoggerBase.h
@@ -11,22 +11,23 @@ enum LOG_LEVEL {
     ERROR = 0, WARNING = 1, INFO = 2, DEBUG = 3
 };
 
+/**
+ * Abstract class
+ */
 class LoggerBase {
 public:
     LoggerBase(LOG_LEVEL maxLogLevel, String formatter);
 
     virtual void logMessage(LOG_LEVEL logLevel, String message) = 0;
     bool isMsgLoggable(LOG_LEVEL msgLogLevel); // Returns true if message loglevel is below the max log level
-
-
-private:
-    String formatter;
-    LOG_LEVEL maxLogLevel;
-
     String getFormatter();
     void setFormatter(String formatter);
     LOG_LEVEL getMaxLogLevel();
     void setMaxLogLevel(LOG_LEVEL logLevel);
+
+private:
+    String formatter;
+    LOG_LEVEL maxLogLevel;
 };
 
 
