1580c58b8e18b9c1cb14ada9dac67c632d8bb0ac
Daniel Copley <djcopley@users.noreply.github.com>
2019-04-27 23:36:32 -0700

    Update Motor.cpp
    Motor:
         conditions:
              robot is tracking
    	move backwards first
    	self balancing but not complete balanced
    	obstacle is forward
    	        complete self balancing
    	        speed up one wheel to turn to another direction
              robot is not tracking or smaller than an exact distance from robot to aim location
    	complete self balancing
    
    tips:
    set dirPins and pwm (pulse width modulation) as outputs
    DirPin 10: clockwise; 01: conterclockwise; 00 || 11: break ( use digitalWrite() ) direction control
    parameters: tilt angle, isTracking
    PWM range is from -255 to 255 ( use analogWrite() ) speed control
    parameters: isTracking (boolean), tiltAngle (double), avoidObstacle (boolean), 4 pins.

--- Motor.cpp
+++ Motor.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Motor.h"
+Motor::Motor(bool isTracking, double tiltAngle, bool avoidObstacle, uint8_t leftPWNPin, uint8_t rightPWNPin, uint8_t leftDirPin,
+             uint8_t rightDirPin)
+: isTrack(isTracking), tiltAngle(tiltAngle), needAvoid(avoidObstacle), leftPWNPin(leftPWNPin),
+rightPWNPin(rightPWNPin), leftDirPin(leftDirPin), rightDirPin(rightDirPin)
+{
+    if(isTrack)
+    {
+        //set speed//
+        analogWrite(leftPWNPin, 100); // need to figure out the relationship between impact and speed.
+        analogWrite(rightPWNPin, 100); 
+
+        //move backward(counterclockwise)//
+        digitalWrite(leftDirPin, HIGH);
+        digitalWrite(rightDirPin, LOW);
+        selfBalancing(10); // need to figure out.
+        if(needAvoid)
+        {
+            selfBalancing(0); //no impacts
+            analogWrite(leftPWNPin, 170); // turn right.
+        }
+    }
+    else
+    {
+        selfBalancing(0); //no impacts
+    }
+}
+void Motor::selfBalancing(int impact)
+{
+    //example: https://www.instructables.com/id/Arduino-Self-Balancing-Robot-1/
+}
